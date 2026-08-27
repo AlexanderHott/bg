25a51b7d0fb6475d085dd1f57f323339ebb55ccf
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2026-08-26 23:20:08 -0700

    format drizzle setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-import { envServer } from './src/envServer';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+import { envServer } from "./src/envServer";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
     url: envServer.DATABASE_URL,
   },
